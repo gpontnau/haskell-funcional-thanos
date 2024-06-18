ad71ecf6dba7003fd60cea9efccf0c014b4d726c
gpontnau <gpontnau@frba.utn.edu.ar>
2024-06-18 18:13:11 -0300

    These changes enhance the functionality of the code and provide more options for manipulating characters in the universe.

--- src/Library.hs
+++ src/Library.hs
@@ -8,13 +8,12 @@ doble numero = numero + numero
 
 data Guantelete = Guantelete {
     material :: String,
-    gemas    :: [String] 
+    gemas    :: [Gema] 
 } deriving Show
-
 data Personaje = Personaje {
     edad           :: Number,
     energia        :: Energia,
-    habilidad    :: Habilidad,
+    habilidad      :: Habilidad,
     nombre         :: String,
     vivenEnPlaneta :: Planeta
 } deriving Show
@@ -24,7 +23,7 @@ type Habilidad = [String]
 type Planeta = String
 type Universo = [Personaje]
 
-guanteleteCompleto :: Guantelete -> Bool-- Funcion auxiliar
+guanteleteCompleto :: Guantelete -> Bool -- Funcion auxiliar
 guanteleteCompleto guantelete = ((== 6) . length . gemas) guantelete && ((=="uru") . material) guantelete
 
 chasquearUniverso :: Guantelete -> Universo -> Universo
@@ -41,49 +40,57 @@ aptoParaPendex personaje = any ((< 45) . edad) personaje
 -- Saber la energía total de un universo que es la sumatoria de todas las energías de
 -- sus integrantes que tienen más de una habilidad
 energiaTotalUniverso :: Universo -> Energia
-energiaTotalUniverso personaje = sum (map energia (filter ((> 1) . length . habilidad) personaje))
-
+energiaTotalUniverso = sum . map energia . filter ((> 1) . length . habilidad)
+-- con map transformas la lista de personajes en una lista de energias
 
 type Gema = Personaje -> Personaje
 
 -- laMente tiene la habilidad de debilitar la energía de un usuario en un valor dado
 laMente :: Number -> Gema
-laMente cuantaEnergiaSaca personaje = personaje { energia = ((cuantaEnergiaSaca-) . energia) personaje }
+laMente = quitarEnergia
+
+quitarEnergia :: Number -> Personaje -> Personaje -- Funcion auxiliar
+quitarEnergia cuantaEnergiaSaca personaje = personaje { energia = energia personaje - cuantaEnergiaSaca }
+
 
 -- elAlma permite eliminar una habilidad en particular si es que la posee. Además le quita 10 puntos de energía.
 elAlma :: String -> Gema
-elAlma habilidadAQuitar personaje = personaje { energia     = ((10-) . energia) personaje,
-                                                habilidad = filter (/= habilidadAQuitar) (habilidad personaje) }
+elAlma habilidadAQuitar personaje = quitarEnergia 10 personaje { 
+    habilidad = filter (/= habilidadAQuitar) $ habilidad personaje
+}
 
 -- elEspacio transporta al rival al planeta x (el que usted decida) y resta 20 puntos de energía
 elEspacio :: Planeta -> Gema
-elEspacio planetaX personaje = personaje { vivenEnPlaneta = planetaX,
-                                           energia = ((20-) . energia) personaje }
+elEspacio planetaX personaje = quitarEnergia 20 personaje { vivenEnPlaneta = planetaX }
 
 -- elPoder deja sin energía al rival y si tiene <=2 habilidad se las quita (en caso contrario no le saca ninguna habilidad).
 elPoder :: Gema
-elPoder personaje
-    | length (habilidad personaje) <= 2 = personaje { energia = 0, habilidad = [] }
-    | otherwise = personaje
+elPoder personaje = atacarHabilidades . quitarEnergia (energia personaje) $ personaje
 
+atacarHabilidades :: Personaje -> Personaje -- Funcion auxiliar
+atacarHabilidades personaje 
+    | (<=2) . length . habilidad $ personaje = quitarHabilidades personaje
+    | otherwise = personaje   
+
+quitarHabilidades:: Gema -- Funcion auxiliar
+quitarHabilidades personaje = personaje { habilidad = [] }
 -- elTiempo reduce a la mitad entera la edad de su oponente pero no puede dejar la edad del oponente con menos de 18 años. 
 -- También resta 50 puntos de energía.
 elTiempo :: Gema
-elTiempo personaje = personaje { edad    = edad personaje `div` 2,
-                                 energia = ((50-) . energia) personaje }
+elTiempo personaje = quitarEnergia 50 personaje { edad = max 18 (edad personaje `div` 2) }
 
 -- La gema loca que permite manipular el poder de una gema y la ejecuta 2 veces contra un rival
 laGemaLoca :: Gema -> Gema
 laGemaLoca gema = gema . gema
 
-
-aplicarGemas :: [Gema] -> Personaje -> Personaje
-aplicarGemas gemas personaje = foldl (flip ($)) personaje gemas
+ 
+aplicarGemas :: [Gema] -> Gema
+aplicarGemas listaGemas enemigo = foldl (flip ($)) enemigo listaGemas
 
 personajePrueba = Personaje {
     edad           = 50,
     energia        = 1000,
-    habilidad    = ["usar Mjolnir", "programación en Haskell"],
+    habilidad      = ["usar Mjolnir", "programación en Haskell"],
     nombre         = "Goncho",
     vivenEnPlaneta = "X"
 }
@@ -100,36 +107,48 @@ Personaje
 
 -- Dar un ejemplo de un guantelete de goma con las gemas tiempo, alma que quita la habilidad de “usar Mjolnir” 
 -- y la gema loca que manipula el poder del alma tratando de eliminar la “programación en Haskell"
-guanteleteDeGoma = Guantelete {
+unGuantelete = Guantelete {
     material = "goma",
-    gemas    = ["Tiempo", "Alma", "GemaLoca"]
+    gemas    = [elTiempo, elAlma "usar Mjolnir", laGemaLoca (elAlma "programación en Haskell")]
 }
 
-guanteleteDeGomaConGemas :: Guantelete -> Personaje -> Personaje
-guanteleteDeGomaConGemas guantelete = 
-    aplicarGemas [elAlma "usar Mjolnir" . laGemaLoca (elAlma "programación en Haskell")]
+-- Generar la función utilizar que dado una lista de gemas y un enemigo 
+-- ejecuta el poder de cada una de las gemas que lo componen contra el personaje dado. 
+utilizar :: [Gema] -> Gema
+utilizar listaGemas enemigo = foldr ($) enemigo listaGemas
+-- Indicar cómo se produce el “efecto de lado” sobre la víctima.
+{-
+En haskell no hay “efecto de lado” sino  que ese personaje se ve afectado por la gema y 
+se crea un nuevo personaje con las características modificadas.
+-}
+
 
 -- Resolver utilizando recursividad. 
 -- Definir la función gemaMasPoderosa que dado un guantelete y una persona obtiene la gema del infinito que
 -- produce la pérdida más grande de energía sobre la persona
---gemaMasPoderosa :: Guantelete -> Personaje -> Gema
---gemaMasPoderosa 
+gemaMasPoderosa :: Guantelete -> Personaje -> Gema
+gemaMasPoderosa guantelete personaje = gemaMasPoderosaRec personaje (gemas guantelete)
+
+gemaMasPoderosaRec :: Personaje -> [Gema] -> Gema
+gemaMasPoderosaRec _ [gema] = gema
+gemaMasPoderosaRec personaje (gema1:gema2:gemas)
+    | (energia.gema1) personaje > (energia.gema2) personaje = gemaMasPoderosaRec personaje (gema2:gemas) 
+    | otherwise = gemaMasPoderosaRec personaje (gema1:gemas)
+
 
 
 {-
-infinitasGemas :: Gema -> [Gema]
-infinitasGemas gema = gema:(infinitasGemas gema)
-
-guanteleteDeLocos :: Guantelete
-guanteleteDeLocos = Guantelete "vesconite" (infinitasGemas tiempo)
-
-Y la función
-
-usoLasTresPrimerasGemas :: Guantelete -> Personaje -> Personaje
-usoLasTresPrimerasGemas guantelete = (utilizar . take 3. gemas) guantelete2
-
 Justifique si se puede ejecutar, relacionándolo con conceptos vistos en la cursada:
 ● gemaMasPoderosa punisher guanteleteDeLocos3 
+Revienta porque la lista de gemas es infinita y Haskell no puede evaluarla. DIVERGE
 
 ● usoLasTresPrimerasGemas guanteleteDeLocos punisher
 -}
+infinitasGemas :: Gema -> [Gema]  -- lista infinita de una determinada gema
+infinitasGemas gema = gema:infinitasGemas gema
+
+guanteleteDeLocos :: Guantelete
+guanteleteDeLocos = Guantelete "vesconite" (infinitasGemas elTiempo)
+
+usoLasTresPrimerasGemas :: Guantelete -> Personaje -> Personaje
+usoLasTresPrimerasGemas guantelete = (utilizar . take 3. gemas) unGuantelete
